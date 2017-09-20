d2d94a9dedac59c87b52445075b0ba8ce7972edd
Baruch <admin@Baruch-Tabanpour.local>
2017-09-19 22:27:58 -0400

    upload script to run trpo experiments

--- baselines/trpo_mpi/run_mujoco.py
+++ baselines/trpo_mpi/run_mujoco.py
@@ -38,11 +38,14 @@ def train(env_id, num_timesteps, seed):
 
 def main():
     import argparse
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--env', help='environment ID', default='Hopper-v1')
     parser.add_argument('--seed', help='RNG seed', type=int, default=0)
+    parser.add_argument(
+        '--num-timesteps', help='num-timesteps', type=int, default=1e6)
     args = parser.parse_args()
-    train(args.env, num_timesteps=1e6, seed=args.seed)
+    train(args.env, num_timesteps=args.num_timesteps, seed=args.seed)
 
 
 if __name__ == '__main__':
